af43162d1671dc385b5c85ac5f4b1cf565c743ca
Mackenzie Weaver <macknz7@gmail.com>
2025-02-28 14:42:31 -0500

    light mode - black to white | dark mode - white to black

--- js/color-scheme.js
+++ js/color-scheme.js
@@ -2,18 +2,6 @@ const root = document.querySelector(':root')
 
 document.querySelector('#color-scheme')
     .addEventListener('change', e => {
-        switch(e.target.value) {
-            case 'light': {
-                root.style['color-scheme'] = 'light'
-                break
-            }
-            case 'dark': {
-                root.style['color-scheme'] = 'dark'
-                break
-            }
-            default: {
-                root.style['color-scheme'] = 'light dark'
-            }
-        }
+        root.style['color-scheme'] = e.target.value
     })
     
---
+++ js/cursor.js
@@ -0,0 +1,12 @@
+const cursor = document.querySelector('#cursor')
+
+const size = 60
+cursor.style.width = size + 'px'
+cursor.style.height = size + 'px'
+cursor.style.borderRadius = size + 'px'
+
+document.addEventListener('mousemove', e => {
+    const { clientX: x, clientY: y } = e
+    cursor.style.left = x - (size / 2) + 'px'
+    cursor.style.top = y - (size / 2) + 'px'
+})
